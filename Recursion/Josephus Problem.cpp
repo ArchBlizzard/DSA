99618c0defcdac4d39e943e117cc955adbf2fb69
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-06 01:14:49 +0530

    Create Josephus Problem.cpp

---
+++ Recursion/Josephus Problem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    int josephus(int n, int k)
+    {
+       if(n == 1) return 1;
+       else return (josephus(n - 1, k) + k-1) % n + 1;
+    }
+};
