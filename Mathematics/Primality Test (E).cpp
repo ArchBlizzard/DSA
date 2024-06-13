4401b2cf55c3c6c33e80428d13f7103bc68f92ea
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-13 21:58:38 +0530

    Create Primality Test (E).cpp

---
+++ Mathematics/Primality Test (E).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        bool isPrime(int N)
+    {
+        if(N <= 1){
+            return false;
+        }
+        if(N == 2 || N == 3){
+            return true;
+        }
+        if(N % 2 == 0 || N % 3 == 0){
+            return false;
+        }
+        for(int i=5; i*i<=N; i=i + 6){
+            if(N % i == 0 || N % (i + 2) == 0){
+                return false;
+            }
+        }
+        return true;//Your code here
+    }
+
+};
