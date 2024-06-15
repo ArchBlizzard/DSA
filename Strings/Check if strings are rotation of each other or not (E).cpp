68a71c20f765bb2434facb0b0d6233c41bdf04a6
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-16 00:43:43 +0530

    Create Check if strings are rotation of each other or not (E).cpp

---
+++ Strings/Check if strings are rotation of each other or not (E).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        if (s1.length() != s2.length())
+            return false;
+
+        string temp = s1 + s1; 
+        return (temp.find(s2) != string::npos);
+    }
+};
