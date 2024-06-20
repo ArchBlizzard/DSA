a4fd3a2ca4a7af15b13b0ff12e136fd40c322ee5
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-20 16:25:46 +0530

    Create Detect Loop in Linked List (M).cpp

---
+++ Linked List/Detect Loop in Linked List (M).cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        unordered_set<Node*> s;
+	    while (head != NULL) {
+		    if (s.find(head) != s.end())
+			    return true;
+		    s.insert(head);
+
+		    head = head->next;
+	    }
+
+	    return false;
+    }
+};
