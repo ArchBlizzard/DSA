b97c7fbe93be4508af0e5c515ddefd4b6d568999
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-25 00:00:42 +0530

    Update Balanced Tree Check (E).cpp

--- Tree/Balanced Tree Check (E).cpp
+++ Tree/Balanced Tree Check (E).cpp
@@ -79,3 +79,57 @@ Node* buildTree(string str) {
 
     return root;
 }
+/* A binary tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    bool isBalanced(Node *root)
+    {
+        int lh;
+
+	// for height of right subtree
+	    int rh;
+
+	// If tree is empty then return true
+	    if (root == NULL)
+		    return 1;
+
+	// Get the height of left and right sub trees
+	    lh = height(root->left);
+	    rh = height(root->right);
+
+	    if (abs(lh - rh) <= 1 && isBalanced(root->left)
+		    && isBalanced(root->right))
+		    return 1;
+
+	// If we reach here then tree is not height-balanced
+	    return 0;//  Your Code here
+    }
+    private:
+    int height(Node* node)
+    {
+	// base case tree is empty
+	    if (node == NULL)
+		    return 0;
+
+	// If tree is not empty then
+	// height = 1 + max of left height
+	// and right heights
+	    return 1 + max(height(node->left), height(node->right));
+    }
+    
+};
