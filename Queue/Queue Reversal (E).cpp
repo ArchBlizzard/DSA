87e8634efb47f244b909185a4f67ec9e204ca21b
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-06-29 00:19:39 +0530

    Create Queue Reversal (E).cpp

---
+++ Queue/Queue Reversal (E).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> s;
+
+        while (!q.empty()) {
+            s.push(q.front());
+            q.pop();
+        }
+
+        while (!s.empty()) {
+            q.push(s.top());
+            s.pop();
+        }
+
+        return q;
+    }
+};
