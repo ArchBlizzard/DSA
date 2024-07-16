3812ac74b9b9f8faed51e0f12feb89a63d89af82
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-07-17 00:57:57 +0530

    Create Count Only Repeated (M).cpp

---
+++ Searching/Count Only Repeated (M).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    //Function to find repeated element and its frequency.
+    pair<long, long> findRepeating(long *arr, int n){
+        long repeating_element = -1;
+        long frequency = 1;
+        
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                repeating_element = arr[i];
+                break;
+            }
+        }
+        
+        if (repeating_element == -1) {
+            return {-1, -1};
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == repeating_element) {
+                frequency++;
+            }
+        }
+        
+        return {repeating_element, frequency - 1};//code here
+    }
+};
