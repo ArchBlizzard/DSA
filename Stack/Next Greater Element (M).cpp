3c2c7ef5a8874360471ca6db582e61cf47e5fb7b
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-07-24 01:24:50 +0530

    Create Next Greater Element (M).cpp

---
+++ Stack/Next Greater Element (M).cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        vector<long long> result(n, -1);
+        stack<long long> s;
+
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && arr[s.top()] < arr[i]) {
+                result[s.top()] = arr[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+
+        return result;// Your code here
+    }
+};
