243edf3213e42c361be1325a6271e091adbf9910
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-09-04 00:25:46 +0530

    Create DFS of Graph (E).cpp

---
+++ Graph/DFS of Graph (E).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void dfs(int node, vector<int> adj[], vector<bool> &visited, vector<int> &result) {
+        visited[node] = true;
+        result.push_back(node);
+        
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, result);
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> result;
+        vector<bool> visited(V, false);
+        dfs(0, adj, visited, result);
+        return result;
+    }
+};
