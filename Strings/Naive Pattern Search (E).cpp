583d10421e428158972b57ec5d4c0b6a71112d4f
ArchBlizzard <85964227+ArchBlizzard@users.noreply.github.com>
2024-09-17 00:00:05 +0530

    Create Naive Pattern Search (E).cpp

---
+++ Strings/Naive Pattern Search (E).cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+class Solution
+{
+    public:
+    //Function to check if the given pattern exists in the given string or not.
+    bool search(string pat, string txt) 
+    { 
+    	
+    	int m = pat.size();
+        int n = txt.size();
+        
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (txt[i + j] != pat[j])
+                    break;
+            }
+            if (j == m)
+                return true;
+        }
+        return false;
+    	
+    }
+};
