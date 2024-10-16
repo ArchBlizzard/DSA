74473be776edfb72ac69f1ccfe2dd03e02025038
Anchit Joshi <anchit2710@gmail.com>
2024-10-17 03:25:02 +0530

    Update Find the number of islands (M).cpp

--- Graph/Find the number of islands (M).cpp
+++ Graph/Find the number of islands (M).cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
+
 class Solution {
+
   public:
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
